3b533a43470266a6bd5966b2d403fcd46f7138b2
leonardoagg <aggioleonardo95@gmail.com>
2021-07-28 12:45:29 +0200

    input generator

---
+++ input_generator/input_gen.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>     
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+
+/*
+Execute the program passing the number 
+of elementes in the sequence
+*/
+
+int main(int argc, char *argv[]) 
+{
+
+  int len = atoi(argv[1]);
+  int sequence[len];
+
+  /* initialize random seed: */
+  srand (time(NULL));
+
+  // output file creation
+  ofstream outfile("inputSequence.txt");
+
+  // generation of txt with 1 number for each row
+  for(int i = 0 ; i < len ; i++)
+  {
+    outfile << rand() << endl;
+  }
+
+  //close file
+  outfile.close();
+
+  return 0;
+}
